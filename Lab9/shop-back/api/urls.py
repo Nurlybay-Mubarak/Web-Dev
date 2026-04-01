70a2539e3c14d04a93b52be739b401962f187c48
Nurlybay-Mubarak <n_mubarak@kbtu.kz>
2026-04-01 18:10:21 +0500

    Lab9 final

---
+++ Lab9/shop-back/api/urls.py
@@ -0,0 +1,10 @@
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
+
+urlpatterns = router.urls
